7baffb7a9496d76c5b001d983109bf40c0111492
rolando <rolando22_@outlook.com>
2023-07-24 20:00:42 -0500

    initial commit update

--- controller/Resecion.js
+++ controller/Resecion.js
@@ -349,7 +349,7 @@ async function postApiWhasatapp({
   addres,
 }) {
   const formData = new URLSearchParams();
-  formData.append("body", "reserva");
+  formData.append("body", "check_reserva_v2");
   formData.append(
     "token",
     "1c38cf1f1b92656924501747a458e4a6b5ac30306d29ed668f9bd8f99f2832fc6ee451"
