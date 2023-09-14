9c1c7e8ea51f25a7caed9ae9b4a4c624306e371a
GRUPO HOTLES <grupohotles@MacBook-Pro-Anderson.local>
2023-09-14 08:59:33 -0500

    initial commit update index

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const { Server } = require("socket.io");
 dbConnection();
 //connection database
 
-//const server = http.createServer(app);
+const server = http.createServer(app);
 
 app.use(express.static('public'));
 app.use(express.json());
@@ -26,8 +26,34 @@ app.use("/api/admin", AdminRoute.router);
 app.use("/api/resecion", ResecionRoute.router);
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
-var port_number = app.listen(process.env.PORT || 5000);
+io.on("connection", (socket) => {
+  console.log("conectado")
+  socket.on("newUser", (username) => {
+    addNewUser(username, socket.id);
+  });
 
+  socket.on("sendNotification", (senderName) => {
+    io.emit("sendNotification", senderName);
+  });
+
+  socket.on("ExitPms", (senderName) => {
+    io.emit("ExitPms", senderName);
+  });
+
+});
+
+server.listen(app.listen(process.env.PORT || 5000, () => {
+  console.log("SERVER IS RUNNING");
+}))
+
+
+//var port_number = app.listen(process.env.PORT || 5000);
 //initial port
-app.listen(port_number);
+//app.listen(port_number);
