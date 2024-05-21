0f7e0628ac0d3408306eea79763d739f197c67ec
Jhonathan Velez <kokoa.arte@gmail.com>
2024-05-21 16:54:50 -0500

    intial commit udate

--- Routes/RouteCloudbeds.js
+++ Routes/RouteCloudbeds.js
@@ -1,7 +1,11 @@
-const { GetHotels } = require('../controller/ControllerCloudbeds/ControllerCloudbeds');
+const { GetHotels, GetHotelsbyID, GetReservationBypropertyID } = require('../controller/ControllerCloudbeds/ControllerCloudbeds');
 
 const router = require('express').Router()
 
 router.get("/getHotel",GetHotels);
+
+router.get("/getHotelbyID/:id",GetHotelsbyID);
+
+router.get("/getReservationBypropertyID/:id",GetReservationBypropertyID);
 
 module.exports = { router };  
--- controller/ControllerCloudbeds/ControllerCloudbeds.js
+++ controller/ControllerCloudbeds/ControllerCloudbeds.js
@@ -25,8 +25,82 @@ const GetHotels =async(req,res=response) =>{
             ok:false
         })
     }
-
 }
+
+const GetHotelsbyID =async(req,res=response) =>{
+
+    const {id} = req.params
+
+    try {
+
+    const response = await fetch(`https://api.cloudbeds.com/api/v1.1/getHotelDetails?propertyID=${id}`, {
+            method: "GET",
+            headers: { 'Content-type': 'application/json',
+            'Authorization': `Bearer cbat_IQWnqN9nTQLBc1mqd8DrCK8CiKbYjXiV` }
+        });
+
+        if (response.status === 401) {
+            return res.status(401).json({ ok: false });
+        }
+        
+        const {data} = await response.json();
+
+        return res.status(201).json({
+            ok:true,
+            data
+        })
+
+    } catch (error) {
+        return res.status(401).json({
+            ok:false
+        })
+    }
+}
+
+const GetReservationBypropertyID =async(req,res=response) =>{
+
+    const {id} = req.params
+
+    try {
+
+    const response = await fetch(`https://api.cloudbeds.com/api/v1.1/getReservations?propertyID=${id}`, {
+            method: "GET",
+            headers: { 'Content-type': 'application/json',
+            'Authorization': `Bearer cbat_IQWnqN9nTQLBc1mqd8DrCK8CiKbYjXiV` }
+        });
+
+        if (!response) {
+            // If access is denied, return 401 status code
+            if (response.status === 401) {
+                return res.status(401).json({ ok: false });
+            }
+            // For other errors, return 500 status code
+            return res.status(401).json({ ok: false });
+        }
+
+        const {data} = await response.json();
+
+        if(!data){
+            return res.status(401).json({
+                ok:false
+            })
+        }
+
+        return res.status(201).json({
+            ok:true,
+            data
+        })
+
+    } catch (error) {
+        return res.status(401).json({
+            ok:false
+        })
+    }
+}
+
+
 module.exports ={
-    GetHotels
+    GetHotels,
+    GetHotelsbyID,
+    GetReservationBypropertyID
 }
