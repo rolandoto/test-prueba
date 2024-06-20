541adedd710161807bf2e671e23b5e7c342ae6dd
Jhonathan Velez <kokoa.arte@gmail.com>
2024-06-20 18:16:01 -0500

    initial commit

--- controller/Controller.js
+++ controller/Controller.js
@@ -40,19 +40,25 @@ const LoginUsuario = async (req,res= response) => {
         // Obtener información adicional del hotel
         const hotelInfoQuery = await pool.query("SELECT name, id, logo, Iva FROM hotels WHERE id = ?", [hotel]); 
         
-        const dian = await pool.query("SELECT observations, ID_DIAN,id_paymen,id_type_document FROM Dian_register WHERE id_hotel = ?", [hotel]);  
+        const dian = await pool.query("SELECT observations, ID_DIAN,id_paymen,id_type_document,Code FROM Dian_register WHERE id_hotel = ?", [hotel]);  
 
         let ID_Dian=0
         let id_payment =0
         let id_document=0
         let observation=""
+
+
+        const RestDian = dian.map((item) =>{
+            return item
+        })
+
         if(dian.length !== 0) {
            ID_Dian  = dian[0].ID_DIAN
            id_payment  = dian[0].id_paymen
            id_document= dian[0].id_type_document
            observation= dian[0].observations
         }
-
+    
         const token = "sdassasadsadsajhaskdjaskdjkashj"
 
         return res.status(201).json({
@@ -71,7 +77,8 @@ const LoginUsuario = async (req,res= response) => {
                 id_payment,
                 id_document,
                 observation,
-                token
+                token,
+                RestDian
             }
         })
 
