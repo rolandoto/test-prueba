94427784cfe182e6b173be7fce61aa6564de8598
Jhonathan Velez <kokoa.arte@gmail.com>
2024-10-09 13:34:24 -0500

    initialcommit

--- controller/ControllerCloudbeds/ControllerCloudbeds.js
+++ controller/ControllerCloudbeds/ControllerCloudbeds.js
@@ -1254,9 +1254,33 @@ const webhooksAdd_Guest =async(req,res=response) =>{
                     guestID:guestID,
                     reservationID:reservationID
                 }
+            
+            
+                
+            const response = await fetch(`https://api.cloudbeds.com/api/v1.1/getGuest?propertyID=${webhookEvent.propertyID}&reservationID=${webhookEvent.reservationID}&guestID=${guestID}`, {
+                method: "GET",
+                headers: { 'Content-type': 'application/json',
+                'Authorization': `Bearer ${hotelInfoQuery[0].Token}` }
+            });
+            
+                if (!response) {
+                    // If access is denied, return 401 status code
+                    if (response.status === 401) {
+                        return res.status(401).json({ ok: false });
+                    }
+                    // For other errors, return 500 status code
+                    return res.status(401).json({ ok: false });
+                }
+            
+                const {data} = await response.json();
+        
+                if(!data){
+                    return res.status(401).json({
+                        ok:false
+                    })
+                }
 
-                console.log(bodyGuest)
-                console.log({"guest":guest})
+                console.log({"sdsadasd":data})
 
                 await pool.query('SELECT * FROM Guest_cloudbed WHERE guestID = ?', [guestID], (selectError, results) =>{
                     if (selectError) {
